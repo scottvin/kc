35b1d3774bd42589f998e7e77b657f6fb57616b0
Vincent Scott <vince.a.scott@gmail.com>
2024-01-28 18:00:22 -0600

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,87 +5,35 @@ import kotlinx.coroutines.coroutineScope
 import java.time.Instant
 
 
-const val seed = 1L shl 51
 const val SIZE = 7
 suspend fun main() {
     cards.forEach { it.init() }
-    val start = Instant.now().toEpochMilli()
-    val cnt = generate7()/*.onEach { println(it.code()) }*/.count()
-//    val cnt3 = list2.onEach { println(it) }.count()
-//    val cnt3 = cards.onEach { println(it) }.count()
-    val end = Instant.now().toEpochMilli() - start
-    println(cnt)
-    println(end)
-    val start3 = Instant.now().toEpochMilli()
-    val cnt3 = generate7()/*.onEach { println(it.code()) }*/.count()
-//    val cnt3 = list2.onEach { println(it) }.count()
-//    val cnt3 = cards.onEach { println(it) }.count()
-    val end3 = Instant.now().toEpochMilli() - start3
-    println(cnt3)
-    println(end3)
+//    val start1 = Instant.now().toEpochMilli()
+//    val cnt1 = generateSequence()/*.onEach { println(it.code()) }*/.count()
+    val cnt1 = generateSequence()/*.onEach { println(it.code()) }*/.toList()
+//    val end1 = Instant.now().toEpochMilli() - start1
+//    println(cnt1)
+//    println(end1)
 }
-
-suspend fun generate7(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal6(it) }
+suspend fun generateSequence(): Sequence<Hand> {
+    return initGenerateInternal()
+        .flatMap { generateSequenceInternal(it) }
+        .flatMap { generateSequenceInternal(it) }
+        .flatMap { generateSequenceInternal(it) }
+        .flatMap { generateSequenceInternal(it) }
+        .flatMap { generateSequenceInternal(it) }
+        .flatMap { generateSequenceInternal(it) }
 }
-
-suspend fun generate6(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal5(it) }
+private fun generateSequenceInternal(parent: Hand): Sequence<Hand> {
+    return parent.card.cards!!.asSequence().map { createHand(parent, it) }
 }
-
-private fun generateInternal6(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .flatMap { generateInternal5(createHand(parent, it)) }
-}
-
-suspend fun generate5(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal4(it) }
-}
-
-private fun generateInternal5(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .flatMap { generateInternal4(createHand(parent, it)) }
-}
-
-suspend fun generate4(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal3(it) }
-}
-
-private fun generateInternal4(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .flatMap { generateInternal3(createHand(parent, it)) }
-}
-
-suspend fun generate3(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal2(it) }
-}
-
-private fun generateInternal3(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .flatMap { generateInternal2(createHand(parent, it)) }
-}
-
-suspend fun generate2(): List<Hand> {
-    return generate1().flatMapAsync { generateInternal1(it) }
-}
-
-private fun generateInternal2(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .flatMap { generateInternal1(createHand(parent, it)) }
-}
-
-fun generate1(): List<Hand> {
-    return cards.map { createHand(null, it) }
-}
-
-private fun generateInternal1(parent: Hand): List<Hand> {
-    return parent.card.cards!!
-        .map { createHand(parent, it) }
+private fun initGenerateInternal(): Sequence<Hand> {
+    return cards.asSequence().map { Hand(null, it.key, it) }
 }
 
 
-suspend fun List<Hand>.flatMapAsync(mapper: suspend (Hand) -> List<Hand>): List<Hand> =
-    coroutineScope { map { async(Dispatchers.Default) { mapper(it) } }.awaitAll().flatten() }
+//suspend fun List<Hand>.flatMap(mapper: suspend (Hand) -> List<Hand>): List<Hand> =
+//    coroutineScope { map { async(Dispatchers.Default) { mapper(it) } }.awaitAll().flatten() }
 
 data class Hand(val parent: Hand?, val key: Long, val card: Card)
 data class Rank(val key: Long, val code: String)
@@ -125,14 +73,13 @@ fun Card.next(card: Card): Card {
     return card
 }
 
-fun createHand(parent: Hand?, card: Card): Hand {
-    val key = if (parent == null) card.key else parent.key or card.key
+fun createHand(parent: Hand, card: Card): Hand {
+    val key = parent.key or card.key
     return Hand(parent, key, card)
 }
 
 fun cards(key: Long): List<Card> = cards.asSequence().filter { (it.key and key) == it.key }.toList()
 fun card(key: Long): Card? = cards.asSequence().filter { (it.key and key) == it.key }.firstOrNull()
 fun Hand.code(): String = cards(this.key).map { it.code }.joinToString(" ")
-fun Card.init(){
-    this.cards = generateSequence ( this.next ){ it.next }.toList()
+fun Card.init(){ this.cards = generateSequence ( this.next ){ it.next }.toList()
 }
