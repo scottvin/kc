ba16331512c579111c6d8b6cd026a3a6a80b2c3c
Vincent Scott <vince.a.scott@gmail.com>
2024-01-30 01:54:35 -0600

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,99 +1,92 @@
 import java.time.Instant
 
 
-suspend fun main() {
-    cards.forEach { it.init() }
+fun main() {
+    init()
     val start1 = Instant.now().toEpochMilli()
-    val cnt1 = generateSequence()/*.onEach { println(it.code()) }*/.toList()
+    val cnt1 = generateSequence()/*.onEach { println(it.code()) }*/.count()
     val end1 = Instant.now().toEpochMilli() - start1
-//    println(cnt1)
+    println(cnt1)
     println(end1)
 }
-
+fun init(){
+    Card.empty.init()
+    Card.collection.forEach { it.init() }
+}
 fun generateSequence(): Sequence<Hand> {
-    return initGenerateInternal()
-        .flatMap { generateSequenceInternal(it) }
-        .flatMap { generateSequenceInternal(it) }
-        .flatMap { generateSequenceInternal(it) }
-        .flatMap { generateSequenceInternal(it) }
-        .flatMap { generateSequenceInternal(it) }
-        .flatMap { generateSequenceInternal(it) }
-//        .flatMap { generateSequenceInternal(it) }
+    return sequenceOf(Hand.root)
+        .flatMap { it.children }
+        .flatMap { it.children }
+        .flatMap { it.children }
+        .flatMap { it.children }
+        .flatMap { it.children }
+        .flatMap { it.children }
+        .flatMap { it.children }
 }
 
-private fun generateSequenceInternal(parent: Hand): Sequence<Hand> {
-    return parent.card.cards!!.map { createHand(parent, it) }
+data class Hand(val key: Long = 0, val card: Card = Card.empty) {
+    companion object {
+        val root:Hand = Hand()
+    }
+    val children: Sequence<Hand> get() = card.remaining.asSequence().map { child(it) }
+    private fun child(card: Card): Hand {
+        return Hand(this.key or card.key, card)
+    }
+    fun code(): String = Card.cards(this.key).joinToString(" ") { it.code }
 }
-
-private fun initGenerateInternal(): Sequence<Hand> {
-    return cards.map { Hand(null, it.key, it) }
+data class Rank(val key: Long = 0, val code: String = "") {
+    companion object {
+        private const val topRankKey = 0b1111L
+        var collection: List<Rank> = listOf(
+            Rank(topRankKey shl (12 * 4), "A"),
+            Rank(topRankKey shl (11 * 4), "K"),
+            Rank(topRankKey shl (10 * 4), "Q"),
+            Rank(topRankKey shl (9 * 4), "J"),
+            Rank(topRankKey shl (8 * 4), "T"),
+            Rank(topRankKey shl (7 * 4), "9"),
+            Rank(topRankKey shl (6 * 4), "8"),
+            Rank(topRankKey shl (5 * 4), "7"),
+            Rank(topRankKey shl (4 * 4), "6"),
+            Rank(topRankKey shl (3 * 4), "5"),
+            Rank(topRankKey shl (2 * 4), "4"),
+            Rank(topRankKey shl (1 * 4), "3"),
+            Rank(topRankKey shl (0 * 4), "2"),
+        )
+    }
+    val cards: List<Card> get() = Suit.collection.map{ Card.create(this, it) }
 }
-
-data class Hand(val parent: Hand?, val key: Long, val card: Card)
-data class Rank(val key: Long, val code: String)
-data class Suit(val key: Long, val code: String)
+data class Suit(val key: Long = 0, val code: String = "") {
+    companion object {
+        private const val topSuitKey = 0b1000100010001000100010001000100010001000100010001000L
+        val collection: List<Suit> = listOf(
+            Suit(topSuitKey shr 0, "S"),
+            Suit(topSuitKey shr 1, "H"),
+            Suit(topSuitKey shr 2, "D"),
+            Suit(topSuitKey shr 3, "C"),
+        )
+    }
+}
 data class Card(
-    val key: Long,
-    val rank: Rank,
-    val suit: Suit,
-    val code: String,
-    var next: Card?,
-    var cards: Sequence<Card>?
-)
-
-var ranks: List<Rank> = listOf(
-    Rank(0b1111L shl (12 * 4), "A"),
-    Rank(0b1111L shl (11 * 4), "K"),
-    Rank(0b1111L shl (10 * 4), "Q"),
-    Rank(0b1111L shl (9 * 4), "J"),
-    Rank(0b1111L shl (8 * 4), "T"),
-    Rank(0b1111L shl (7 * 4), "9"),
-    Rank(0b1111L shl (6 * 4), "8"),
-    Rank(0b1111L shl (5 * 4), "7"),
-    Rank(0b1111L shl (4 * 4), "6"),
-    Rank(0b1111L shl (3 * 4), "5"),
-    Rank(0b1111L shl (2 * 4), "4"),
-    Rank(0b1111L shl (1 * 4), "3"),
-    Rank(0b1111L shl (0 * 4), "2"),
-)
-
-val baseSuitKey = 0b1000100010001000100010001000100010001000100010001000L
-val suits: List<Suit> = listOf(
-    Suit(baseSuitKey shr 0, "S"),
-    Suit(baseSuitKey shr 1, "H"),
-    Suit(baseSuitKey shr 2, "D"),
-    Suit(baseSuitKey shr 3, "C"),
-)
-
-val cards: Sequence<Card> =
-    ranks.flatMap { rank ->
-        suits.map { suit ->
-            Card(
-                rank.key and suit.key,
-                rank,
-                suit,
-                rank.code + suit.code,
-                null,
-                null
-            )
+    val key: Long = Long.MAX_VALUE,
+    val rank: Rank = Rank(),
+    val suit: Suit = Suit(),
+    val code: String = "",
+) {
+    companion object {
+        val empty:Card = Card()
+        val collection: List<Card> = Rank.collection.flatMap { it.cards}
+        fun cards(key: Long): List<Card> = collection.filter { (it.key and key) == it.key }.toList()
+        fun card(key: Long): Card? = collection.firstOrNull { (it.key and key) == it.key }
+        fun create(rank: Rank, suit: Suit): Card {
+            return Card(rank.key and suit.key, rank, suit, rank.code + suit.code)
         }
     }
-        .runningReduce { acc, card -> acc.next(card) }
-        .asSequence()
-
-fun Card.next(card: Card): Card {
-    this.next = card
-    return card
+    lateinit var remaining: List<Card>
+    fun init():Card {
+        this.remaining = collection.filter { it.key < this.key }
+        return this
+    }
 }
 
-fun createHand(parent: Hand, card: Card): Hand {
-    val key = parent.key or card.key
-    return Hand(parent, key, card)
-}
 
-fun cards(key: Long): List<Card> = cards.asSequence().filter { (it.key and key) == it.key }.toList()
-fun card(key: Long): Card? = cards.asSequence().filter { (it.key and key) == it.key }.firstOrNull()
-fun Hand.code(): String = cards(this.key).map { it.code }.joinToString(" ")
-fun Card.init() {
-    this.cards = generateSequence(this.next) { it.next }
-}
+
