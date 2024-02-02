31db283a66372a7e3496a39a833dbe8a5eb8bfed
vscott <vince.a.scott@gmail.com>
2024-02-02 10:29:39 -0600

    init

--- src/main/kotlin/domain/Hand.kt
+++ src/main/kotlin/domain/Hand.kt
@@ -1,18 +1,20 @@
 package domain
 
+import java.util.stream.Stream
+
 data class Hand(val key: Long = 0, val card: Card = Card.empty) {
     companion object {
         val root: Hand = Hand()
     }
-    private val children: Sequence<Hand> get() = card.remaining.asSequence().map { child(it) }
+    private val children: List<Hand> get() = card.remaining.asSequence().map { child(it) }.toList()
     private fun child(card: Card): Hand {
         return Hand(this.key or card.key, card)
     }
     fun code(): String = Card.cards(this.key).joinToString(" ") { it.code }
-    fun children(depth:Int, depthIndex:Int = 0): Sequence<Hand> {
+    fun children(depth:Int, depthIndex:Int = 0): Stream<Hand> {
         if(depthIndex < depth) {
-            return this.children.flatMap { it.children(depth, depthIndex + 1) }
+            return this.children.stream().flatMap { it.children(depth, depthIndex + 1) }
         }
-        return sequenceOf(this)
+        return Stream.of(this)
     }
 }
