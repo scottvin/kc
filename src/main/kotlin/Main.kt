ff4ba70b616472e97fdaae12d02c494692bbbf90
Vincent Scott <vince.a.scott@gmail.com>
2024-02-03 15:22:34 -0600

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,7 @@ import java.time.Instant
 fun main() {
     Card.init()
     val start = Instant.now().toEpochMilli()
-    val cnt = Hand.root.children( 7)
-//        .onEach { println(it.code()) }
-        .count()
+    val cnt = Hand.root.children( 7).count()
     val end = Instant.now().toEpochMilli() - start
     println("Run Count $cnt")
     println("Run Time $end")
--- src/main/kotlin/domain/Hand.kt
+++ src/main/kotlin/domain/Hand.kt
@@ -6,14 +6,14 @@ data class Hand(val key: Long = 0, val card: Card = Card.empty) {
     companion object {
         val root: Hand = Hand()
     }
-    private val children: List<Hand> get() = card.remaining.asSequence().map { child(it) }.toList()
+    private val children: Stream<Hand> get() = card.remaining.stream().map { child(it) }
     private fun child(card: Card): Hand {
         return Hand(this.key or card.key, card)
     }
     fun code(): String = Card.cards(this.key).joinToString(" ") { it.code }
     fun children(depth:Int, depthIndex:Int = 0): Stream<Hand> {
         if(depthIndex < depth) {
-            return this.children.stream().flatMap { it.children(depth, depthIndex + 1) }
+            return this.children.flatMap { it.children(depth, depthIndex + 1) }
         }
         return Stream.of(this)
     }
