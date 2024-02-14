92d23e089b3c962d9315b799d2cc0bb9347491d1
vscott <vince.a.scott@gmail.com>
2024-02-14 06:07:47 -0600

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,17 +9,6 @@ suspend fun main() = runBlocking {
 }
 
 private suspend fun combine() {
-    fun hands(hand: Hand): Sequence<Hand> =
-        sequenceOf(hand)
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .flatMap { it.children() }
-            .map { it.copy(key = 0L, filterKey = it.key, card = Card.empty) }
-
     fun pocket(hand: Hand): Sequence<Hand> =
         sequenceOf(hand)
             .flatMap { it.children() }
@@ -43,28 +32,18 @@ private suspend fun combine() {
             .flatMap { it.children() }
             .map { it.copy(riverKey = it.sub(), card = Card.empty) }
 
+    fun hands(hand:Hand): Sequence<Hand> =  sequenceOf(hand)
+        .flatMap { pocket(it) }
+        .flatMap { flop(it) }
+        .flatMap { turn(it) }
+        .flatMap { river(it) }
+
     fun hands() = flow {
         println(
             measureTimeMillis {
-                sequenceOf(sequenceOf(Hand()))
-                    .map { it.flatMap { hand -> hands(hand) } }
-                    .flatten()
-                    .map { sequenceOf(it) }
-                    .take(100_000)
-                    .map { it.flatMap { hand -> pocket(hand) } }
-                    .map { it.flatMap { hand -> flop(hand) } }
-                    .map { it.flatMap { hand -> turn(hand) } }
-                    .map { it.flatMap { hand -> river(hand) } }
-                    .forEach { emit(it) }
+                hands(Hand()).forEach { emit(it) }
             }
         )
     }
-    println(
-        measureTimeMillis {
-            println("Count ${hands().buffer(1_000_000).map { it.count().toLong() }.reduce { acc, it -> acc + it }}")
-        }
-    )
-    hands().take(1).collect{it.take(1).forEach { hand ->  println(hand.print()) }}
-
-    hands().buffer(1_000).collect { it.forEach { _ -> } }
+    hands().buffer(1_000).collect { }
 }
