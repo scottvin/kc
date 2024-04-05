29782af78252281ce5377925fffb90e21477ad9d
Vincent Scott <vince.a.scott@gmail.com>
2024-04-04 19:59:31 -0500

    pg

--- src/main/kotlin/domain/Hand.kt
+++ src/main/kotlin/domain/Hand.kt
@@ -18,11 +18,12 @@ data class Hand(
             .filter { it.key.or(parentKey) == 0L }
             .map { copy(card = it, pocket = this) }
 }
+val Card.hand: Hand get() = Hand(card = this)
+val Card.filteredHands0:Sequence<Hand> get() = sequenceOf(hand)
+val Card.filteredHands1:Sequence<Hand> get() = filteredHands0.flatMap { it.filterChildren }
+val Card.filteredHands2:Sequence<Hand> get() = filteredHands1.flatMap { it.filterChildren }
 
-val Card.pockets0:Sequence<Hand> get() = sequenceOf(Hand(card = this))
-val Card.pockets1:Sequence<Hand> get() = pockets0.flatMap { it.filterChildren }
-
-val Card.hands0:Sequence<Hand> get() = sequenceOf(Hand(card = this))
+val Card.hands0:Sequence<Hand> get() = sequenceOf(hand)
 val Card.hands1:Sequence<Hand> get() = hands0.flatMap { it.children }
 val Card.hands2:Sequence<Hand> get() = hands1.flatMap { it.children }
 val Card.hands3:Sequence<Hand> get() = hands2.flatMap { it.children }
