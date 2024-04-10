054de98bc45262062cad673af3f1dee465080275
Vincent Scott <vince.a.scott@gmail.com>
2024-04-10 11:01:09 -0500

    val key:Long get() = card.key.or(parent?.key ?: 0)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ suspend fun main() = runBlocking {
     var total = AtomicLong()
     val work = scope.launch {
         Card.collection
-//            .take(1)
+            .take(1)
             .map { card ->
                 async {
                     sequenceOf(Hand(card))
@@ -22,23 +22,20 @@ suspend fun main() = runBlocking {
                         .flatMap { it.children }
                         .flatMap { it.children }
                         .flatMap { it.children }
-//                        .take(1)
+                        .take(1)
                 }
             }
             .awaitAll()
             .asSequence()
             .flatten()
-            .chunked(125_000)
+            .chunked(12_500)
             .map { hands ->
                 async {
                     hands.asSequence()
-                        .flatMap { it.drawsInit }
-                        .flatMap { it.draws }
-                        .flatMap { it.drawsInit }
-                        .flatMap { it.draws }
-                        .flatMap { it.draws }
-                        .flatMap { it.drawsInit }
-                        .flatMap { it.drawsInit }
+                        .flatMap { it.pockets }
+                        .flatMap { it.flops }
+                        .flatMap { it.turns }
+                        .flatMap { it.rivers }
                 }
             }
             .forEach { hands ->
@@ -49,11 +46,15 @@ suspend fun main() = runBlocking {
                         total.addAndGet(count)
                         println(
                             """
-                        |Bass: ${first?.baseKey?.code} 
-                        |Parent: ${first?.parentKey?.code} 
-                        |Draw: ${first?.drawKey?.code} 
-                        |Total: ${total.toLong().format}  
-                        |Count: ${count.format}  
+                        |Bass:    ${first?.baseKey?.code} 
+                        |Parent:  ${first?.parentKey?.code} 
+                        |Draw:    ${first?.drawKey?.code} 
+                        |Pocket:  ${first?.pocketKey?.code} 
+                        |Flow:    ${first?.flopKey?.code} 
+                        |Turn:    ${first?.turnKey?.code} 
+                        |River:   ${first?.riverKey?.code} 
+                        |Total:   ${total.toLong().format}  
+                        |Count:   ${count.format}  
                         |Elapsed: ${time.elapsedNow()}
                         |
                         |""".trimMargin()
@@ -64,6 +65,7 @@ suspend fun main() = runBlocking {
     }
     work.join()
     println("Count: ${total.toLong().format}  Elapsed: ${time.elapsedNow()}")
+
 }
 
 val Card.hand: Hand get() = Hand(card = this)
@@ -71,36 +73,58 @@ val Hand.children: Sequence<Hand>
     get() = card.remaining.asSequence()
         .map { copy(parent = this, baseKey = baseKey.or(it.key), card = it) }
 
-val Hand.cards: List<Card>
+val Hand.initCards: List<Card>
     get() = Card.collection
         .filter { baseKey.and(it.key) == it.key }
         .filter { parentKey.and(it.key) == 0L }
 
+val Hand.cards: List<Card>
+    get() = initCards.filter { it.key < card.key }
+
 val Hand.drawsInit: Sequence<Hand>
-    get() =
-        cards.asSequence()
-            .map {
-                copy(
-                    parent = this,
-                    drawKey = it.key,
-                    parentKey = parentKey.or(it.key),
-                    card = it
-                )
-            }
+    get() = initCards.asSequence()
+        .map { copy(parent = this, drawKey = it.key, parentKey = parentKey.or(it.key), card = it) }
 
 val Hand.draws: Sequence<Hand>
-    get() =
-        cards.asSequence()
-            .filter { it.key < card.key }
-            .map {
-                copy(
-                    parent = this,
-                    drawKey = drawKey.or(it.key),
-                    parentKey = parentKey.or(it.key),
-                    card = it
-                )
-            }
+    get() = cards.asSequence()
+        .map { copy(parent = this, drawKey = drawKey.or(it.key), parentKey = parentKey.or(it.key), card = it) }
 
+val Hand.pockets: Sequence<Hand>
+    get() = sequenceOf(this)
+        .flatMap { it.drawsInit }
+        .flatMap { it.draws }
+        .map { it.copy(pocketKey = it.drawKey) }
+
+val Hand.flops: Sequence<Hand>
+    get() = sequenceOf(this)
+        .flatMap { it.drawsInit }
+        .flatMap { it.draws }
+        .flatMap { it.draws }
+        .map { it.copy(flopKey = it.drawKey) }
+
+val Hand.turns: Sequence<Hand>
+    get() = sequenceOf(this)
+        .flatMap { it.drawsInit }
+        .map { it.copy(turnKey = it.drawKey) }
+
+val Hand.rivers: Sequence<Hand>
+    get() = sequenceOf(this)
+        .flatMap { it.drawsInit }
+        .map { it.copy(riverKey = it.drawKey) }
+
+val Hand.print: Unit
+    get() = println(
+        """
+        |Bass:    ${baseKey.code} 
+        |Parent:  ${parentKey.code} 
+        |Draw:    ${drawKey.code} 
+        |Pocket:  ${pocketKey.code} 
+        |Flow:    ${flopKey.code} 
+        |Turn:    ${turnKey.code} 
+        |River:   ${riverKey.code} 
+        |
+        |""".trimMargin()
+    )
 
 val Long.cards: List<Card> get() = Card.collection.filter { (it.key and this) == it.key }
 val Long.code: String get() = cards.joinToString(" ") { c -> c.code }
--- src/main/kotlin/domain/Hand.kt
+++ src/main/kotlin/domain/Hand.kt
@@ -6,6 +6,10 @@ data class Hand(
     val parentKey: Long = 0L,
     val baseKey: Long = card.key,
     val drawKey: Long = 0L,
+    val pocketKey: Long = 0L,
+    val flopKey: Long = 0L,
+    val turnKey: Long = 0L,
+    val riverKey: Long = 0L,
 ) {
 }
 
