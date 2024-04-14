ea65ee696cb3c4003fee52c77bf946097a11bea2
Vincent Scott <vince.a.scott@gmail.com>
2024-04-14 12:21:58 -0500

    val key:Long get() = card.key.or(parent?.key ?: 0)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,4 @@
-import domain.Card
-import domain.Draw
-import domain.Hand
-import domain.Rank
+import domain.*
 import kotlinx.coroutines.*
 import java.text.DecimalFormat
 import java.util.concurrent.atomic.AtomicLong
@@ -11,219 +8,246 @@ import kotlin.time.TimeSource
 suspend fun main() = runBlocking {
     val time = TimeSource.Monotonic.markNow()
     val scope = CoroutineScope(Dispatchers.Default)
-    var total = AtomicLong()
+    val total = AtomicLong()
     val work = scope.launch {
-        Card.collection
-//            .take(1)
+        Card.collection.asSequence()
             .map { card ->
                 async {
-                    sequenceOf(Hand(card))
+                    sequenceOf(Hand(index = 1, card = card))
                         .flatMap { it.children }
                         .flatMap { it.children }
                         .flatMap { it.children }
                         .flatMap { it.children }
                         .flatMap { it.children }
                         .flatMap { it.childrenLast }
-//                        .filter { it.straightKey.countOneBits() < 5 && it.straightFlushKey.countOneBits() < 5 && it.kindKey.countOneBits() == 0 }
-//                        .filter { hand -> hand.baseKey.and(Rank._A.key).countOneBits() == 1 }
-//                        .filter { hand -> hand.baseKey.and(Rank._K.key).countOneBits() == 1 }
-//                        .filter { hand -> hand.baseKey.and(Rank._Q.key).countOneBits() == 1 }
-//                        .filter { hand -> hand.baseKey.and(Rank._J.key).countOneBits() == 1 }
-//                        .filter { hand -> hand.baseKey.and(Rank._T.key).countOneBits() == 1 }
-//                        .filter { hand -> hand.baseKey.and(Rank._9.key).countOneBits() == 2 }
-//                        .take(1)
+
+//                        .filter { it.baseKey.and(Rank._A.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._K.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._Q.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._J.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._T.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._9.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._8.key).countOneBits() == 1 }
+
+//                        .filter { it.baseKey.and(Rank._A.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._K.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._Q.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._J.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._T.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._9.key).and(Suit._S.key).countOneBits() == 1 }
+//                        .filter { it.baseKey.and(Rank._8.key).and(Suit._S.key).countOneBits() == 1 }
+
+//                        .filter { it.baseKey.and(Rank._A.key).and(Suit._S.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._K.key).and(Suit._C.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._Q.key).and(Suit._D.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._J.key).and(Suit._H.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._T.key).and(Suit._S.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._9.key).and(Suit._C.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._8.key).and(Suit._D.key) > 0 }
+//                        .filter { it.baseKey.and(Rank._7.key).and(Suit._H.key) > 0 }
                 }
             }
-//            .map { hands ->
-//                async {
-//                    hands.await()
-//                        .filter {
-////                            it.kindKey.countOneBits() == 4
-////                                    it.twoKindKey.countOneBits() != 4 &&
-//                                    it.straightKey.countOneBits() == 5
-////                                    it.straightFlushKey.countOneBits() < 5
-////                                    it.flushKey.countOneBits() < 5
-//                        }
-//                }
-//            }
-            .awaitAll()
-            .asSequence()
-            .flatten()
-            .chunked(125_000)
-            .map { hands ->
-                async {
-                    hands.asSequence()
-                        .map {
-                            val key = when {
-                                it.straightFlushKey.countOneBits() >= 5 -> it.copy(draw = Draw.STRAIGHT_FLUSH)
-                                it.quadrupleKey.countOneBits() == 4 -> it.copy(draw = Draw.QUADRUPLE)
-                                it.fullHouseKey.countOneBits() >= 5 -> it.copy(draw = Draw.FULL_HOUSE)
-                                it.flushKey.countOneBits() >= 5 -> it.copy(draw = Draw.FLUSH)
-                                it.triplesKey.countOneBits() == 3 -> it.copy(draw = Draw.TRIPLE)
-                                it.twoPairKey.countOneBits() == 4 -> it.copy(draw = Draw.TWO_PAIR)
-                                it.pairsKey.countOneBits() == 2 -> it.copy(draw = Draw.PAIR)
-                                else -> it
-                            }
-                            key
-                        }
-                }
-            }
-//            .map { hands ->
-//                async {
-//                    hands.await()
-//                        .flatMap { it.pockets }
-//                        .flatMap { it.flops }
-//                        .flatMap { it.turns }
-//                        .flatMap { it.rivers }
-//                }
-//            }
-            .forEach { hands ->
+            .forEach { data ->
                 launch {
-                    hands.await()
-                        .let { hands ->
-                            val count = hands.count().toLong()
-                            total.addAndGet(count)
-                            val first = hands.firstOrNull()
-                            first?.print
-                            println(
-                                """
-                                    |Total:   ${total.toLong().format}
-                                    |Count:   ${count.format}
-                                    |Elapsed: ${time.elapsedNow()}
-                                    |
-                                """.trimMargin()
-                            )
-                        }
+                    data.await().let { hands ->
+                        hands.map {
+                            val draw = when {
+                                it.royalFlushKey >= 5 -> Draw.ROYAL_FLUSH
+                                it.straightFlushBits >= 5 -> Draw.STRAIGHT_FLUSH
+                                it.quadrupleBits == 4 -> Draw.QUADRUPLE
+                                it.fullHouseBits >= 5 -> Draw.FULL_HOUSE
+                                it.flushBits >= 5 -> Draw.FLUSH
+                                it.straightBits >= 5 -> Draw.STRAIGHT
+                                it.triplesBits == 3 -> Draw.TRIPLE
+                                it.twoPairBits == 4 -> Draw.TWO_PAIR
+                                it.pairsBits == 2 -> Draw.PAIR
+                                else -> Draw.HIGH_CARD
+                            }
+                            draw.total.incrementAndGet()
+                            it.copy(draw = draw)
+                        }.forEach { _ -> }
+                        total.addAndGet(hands.count().toLong())
+                    }
                 }
             }
     }
     work.join()
-    println("Count: ${total.toLong().format}  Elapsed: ${time.elapsedNow()}")
+    Draw.collection
+        .forEach { println(
+            """
+                Draw: ${it.name} 
+                Count: ${it.total.toLong().format} 
+                Sequence: ${it.sequence7.format} 
+                Diff: ${(it.total.toLong() - it.sequence7).format}
+                
+                """.trimIndent()) }
 
+    println("Count: ${total.toLong().format}  Elapsed: ${time.elapsedNow()}")
 }
+
+val Hand.drawHands: Sequence<Hand>
+    get() = sequenceOf(this)
+        .flatMap { it.pockets }
+        .flatMap { it.flops }
+        .flatMap { it.turns }
+        .flatMap { it.rivers }
 
 val Hand.children: Sequence<Hand>
     get() = card.remaining.asSequence()
-        .map { copy(parent = this, baseKey = baseKey.or(it.key), card = it) }
+        .map { copy(parent = this, baseKey = baseKey.or(it.key), card = it, index = index + 1) }
 
 val Hand.childrenLast: Sequence<Hand>
     get() = card.remaining.asSequence()
-        .map { copy(parent = this, baseKey = baseKey.or(it.key), card = it, last = true) }
+        .map { copy(parent = this, baseKey = baseKey.or(it.key), card = it, index = index + 1, last = true) }
 
-//val Hand.children: Sequence<Hand>
-//    get() = card.edge.asSequence()
-//        .map {
-//            copy(
-//                parent = this,
-//                baseKey = baseKey.or(it.cardOut.key),
-//                card = it.cardOut,
-//            )
-//        }
-//val Hand.straightFlushKey: Long
-//    get() {
-//        return parent?.let { baseKey.and(card.rank.seriesKey).and(card.suit.key) } ?: 0L
-//    }
-val Hand.straightFlushKey: Long
-    get() {
-        return parent?.let {
-            if(it.straightFlushKey.countOneBits() < 5) {
-                val newStraightFlushKey = baseKey.and(card.rank.seriesKey).and(card.suit.key)
-                if (newStraightFlushKey.countOneBits() > it.straightFlushKey.countOneBits()) {
-                    return newStraightFlushKey
-                }
-            }
-            it.straightFlushKey
-        } ?: 0L
-    }
-
-val Hand.flushKey: Long
-    get() {
-        return parent?.let {
-            if (it.flushKey.countOneBits() < 5) {
-                val newFlushKey = baseKey.and(card.suit.key)
-                if (newFlushKey.countOneBits() > it.flushKey.countOneBits()) {
-                    return newFlushKey
-                }
-            }
-            return it.flushKey
-        } ?: 0L
-    }
-
+val Hand.rankKey: Long get() = baseKey.and(card.rank.key)
+val Hand.rankBits: Int get() = rankKey.countOneBits()
+val Hand.kindBits: Int get() = kindKey.countOneBits()
 val Hand.kindKey: Long
     get() {
-        return parent?.let {
-            if (it.card.rank.key != card.rank.key) {
-                val kindKey = it.baseKey.and(it.card.rank.key)
-                if (kindKey.countOneBits() >= 2) {
-                    return kindKey
+        if (index >= 2) {
+            return parent?.let {
+                if (it.card.rank.key != card.rank.key) {
+                    if (it.rankBits >= 2) {
+                        return it.rankKey
+                    }
+                } else if (last) {
+                    if (rankBits >= 2) {
+                        return rankKey
+                    }
                 }
-            } else if (last) {
-                val kindKey = baseKey.and(card.rank.key)
-                if (kindKey.countOneBits() >= 2) {
-                    return kindKey
+                return 0L
+            } ?: 0L
+        }
+        return 0L
+    }
+val Hand.royalFlushBits: Int get() = royalFlushKey.countOneBits()
+val Hand.royalFlushKey: Long
+    get() {
+        if (index >= 5) {
+            return parent?.let {
+                val newRoyalFlushKey = baseKey.and(Rank.ROYAL_RANKS_KEY).and(card.suit.key)
+                if (it.royalFlushBits < 5 && newRoyalFlushKey.countOneBits() >= 5) {
+                    return newRoyalFlushKey
                 }
-
-            }
-            return it.kindKey
-        } ?: 0L
+                it.royalFlushKey
+            } ?: 0L
+        }
+        return 0L
     }
-val Hand.pairsKey: Long
+val Hand.straightFlushBits: Int get() = straightFlushKey.countOneBits()
+val Hand.straightFlushKey: Long
     get() {
-        return parent?.let {
-            if (kindKey.countOneBits() == 2 && it.pairsKey.countOneBits() < 4) {
-                return it.pairsKey.or(kindKey)
-            }
-            return it.pairsKey
-        } ?: 0L
+        if (index >= 5) {
+            return parent?.let {
+                if (it.straightFlushBits < 5) {
+                    val newStraightFlushKey = baseKey.and(card.rank.seriesKey).and(card.suit.key)
+                    if (newStraightFlushKey.countOneBits() > it.straightFlushBits) {
+                        return newStraightFlushKey
+                    }
+                }
+                it.straightFlushKey
+            } ?: 0L
+        }
+        return 0L
     }
-
-val Hand.triplesKey: Long
-    get() {
-        return parent?.let {
-            if (kindKey.countOneBits() == 3 && it.triplesKey.countOneBits() < 6) {
-                return it.triplesKey.or(kindKey)
-            }
-            return it.triplesKey
-        } ?: 0L
-    }
-
+val Hand.quadrupleBits: Int get() = quadrupleKey.countOneBits()
 val Hand.quadrupleKey: Long
     get() {
-        return parent?.let {
-            if (kindKey.countOneBits() == 4 && it.quadrupleKey.countOneBits() < 4) {
-                return it.quadrupleKey.or(kindKey)
-            }
-            return it.quadrupleKey
-        } ?: 0L
+        if (index >= 4) {
+            return parent?.let {
+                if (kindBits == 4 && it.quadrupleBits < 4) {
+                    return it.quadrupleKey.or(kindKey)
+                }
+                return it.quadrupleKey
+            } ?: 0L
+        }
+        return 0L
     }
 
+val Hand.fullHouseBits: Int get() = fullHouseKey.countOneBits()
 val Hand.fullHouseKey: Long
     get() {
-        return parent?.let {
-            if (it.fullHouseKey.countOneBits() < 5) {
-                return it.fullHouseKey.or(triplesKey).or(pairsKey)
-            }
-            return it.fullHouseKey
-        } ?: 0L
+        if (index >= 5) {
+            return parent?.let {
+                if (it.fullHouseBits < 5) {
+                    return it.fullHouseKey.or(triplesKey).or(pairsKey)
+                }
+                return it.fullHouseKey
+            } ?: 0L
+        }
+        return 0L
     }
 
-val Hand.twoPairKey: Long
+val Hand.flushBits get() = flushKey.countOneBits()
+val Hand.flushKey: Long
     get() {
-        return parent?.let {
-            if (it.twoPairKey.countOneBits() < 4) {
-                return it.twoPairKey.or(pairsKey)
-            }
-            return it.twoPairKey
-        } ?: 0L
+        if (index >= 5) {
+            return parent?.let {
+                if (it.flushBits < 5) {
+                    val newFlushKey = baseKey.and(card.suit.key)
+                    if (newFlushKey.countOneBits() > it.flushBits) {
+                        return newFlushKey
+                    }
+                }
+                return it.flushKey
+            } ?: 0L
+        }
+        return 0L
     }
-
+val Hand.straightBits: Int get() = straightKey.countOneBits()
 val Hand.straightKey: Long
     get() {
         val cardKey = card.key
         return parent?.let {
-            val newStraightKey = it.straightKey.or(it.card.rank.next.key.and(cardKey)).and(card.rank.seriesKey)
-            if (it.straightKey.countOneBits() >= newStraightKey.countOneBits()) it.straightKey else newStraightKey
+            val newStraightKey = it.baseKey.and(card.rank.seriesKey)
+            if(it.straightBits < 5 && newStraightKey.countOneBits() >= 5) {
+                return it.baseKey.and(card.rank.seriesKey)
+            }
+            it.straightKey
         } ?: cardKey
+    }
+
+val Hand.triplesBits: Int get() = triplesKey.countOneBits()
+val Hand.triplesKey: Long
+    get() {
+        if (index >= 3) {
+            return parent?.let {
+                if (kindBits == 3 && it.triplesBits < 6) {
+                    return it.triplesKey.or(kindKey)
+                }
+                return it.triplesKey
+            } ?: 0L
+        }
+        return 0L
+    }
+
+val Hand.twoPairBits: Int get() = twoPairKey.countOneBits()
+val Hand.twoPairKey: Long
+    get() {
+        if (index >= 4) {
+            return parent?.let {
+                if (it.twoPairBits < 4) {
+                    return it.twoPairKey.or(pairsKey)
+                }
+                return it.twoPairKey
+            } ?: 0L
+        }
+        return 0L
+    }
+
+val Hand.pairsBits: Int get() = pairsKey.countOneBits()
+val Hand.pairsKey: Long
+    get() {
+        if (index >= 2) {
+            return parent?.let {
+                if (kindBits == 2 && it.pairsBits < 4) {
+                    return it.pairsKey.or(kindKey)
+                }
+                return it.pairsKey
+            } ?: 0L
+        }
+        return 0L
     }
 
 val Hand.initDrawCards: List<Card>
--- src/main/kotlin/domain/Hand.kt
+++ src/main/kotlin/domain/Hand.kt
@@ -3,11 +3,13 @@ package domain
 import java.io.Flushable
 
 data class Hand(
+    val index:Int,
     val card: Card,
     val parent: Hand? = null,
     val parentKey: Long = 0L,
     val baseKey: Long = card.key,
     val last: Boolean = false,
+    val drawHands: List<Hand> = listOf(),
 
     val drawKey: Long = 0L,
     val pocketKey: Long = 0L,
@@ -15,7 +17,7 @@ data class Hand(
     val turnKey: Long = 0L,
     val riverKey: Long = 0L,
 
-    val draw: Draw= Draw.HIGH_CARD
+    val draw: Draw = Draw.HIGH_CARD
     ) {
 
 }
